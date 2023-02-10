7bc3a42957ae9fe69e96120358907f4d25249232
Abhinav5383 <119372409+Abhinav5383@users.noreply.github.com>
2023-02-10 02:21:40 +0000

    Recreating template for React App

---
+++ .vscode/NavBar.js
@@ -0,0 +1,25 @@
+import React from "react"
+import './NavStyle.css'
+export default function NavBar() {
+  return (
+    <nav className='navBar'>
+      <ul className='itemContainer'>
+
+        <li className='navItem'>
+          <a href='/' className='iconContainer'>
+            <img src={logo} alt='Icon' />
+          </a>
+        </li>
+
+        <li className='navItem'>
+          <a href='/'>Home</a>
+        </li>
+
+        <li className='navItem'>
+          <a href='/'>About</a>
+        </li>
+
+      </ul>
+    </nav>
+  )
+}
